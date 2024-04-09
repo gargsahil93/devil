c5d0d194ef00f13636ad5bb70a8d563832f11bd1
sgargawesome <sgargawesome@penguin>
2024-04-09 07:23:44 +0530

    prettier changes

--- src/app/networth/page.tsx
+++ src/app/networth/page.tsx
@@ -1,14 +1,24 @@
-'use client'
+'use client';
 
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function Page() {
-    const [total, setTotal] = useState('');
-    const submitForm = () => {
-        console.log(total);
-    }
-    return <>
-        Total: <input name="net" type="text" value={total} onChange={(e) => setTotal(e.target.value)} />
-        <button type="submit" onClick={submitForm}>Submit</button>
-    </>;
+  const [total, setTotal] = useState('');
+  const submitForm = () => {
+    console.log(total);
+  };
+  return (
+    <>
+      Total:{' '}
+      <input
+        name="net"
+        type="text"
+        value={total}
+        onChange={(e) => setTotal(e.target.value)}
+      />
+      <button type="submit" onClick={submitForm}>
+        Submit
+      </button>
+    </>
+  );
 }
